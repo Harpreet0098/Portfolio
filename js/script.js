7d37c43e8911be7245ab8085a80f116d22fe7f68
Harpreet0098 <harpreet1905@gmail.com>
2017-02-24 18:22:47 -0500

    about page added

--- js/script.js
+++ js/script.js
@@ -110,3 +110,63 @@ particlesJS("particles-js", {
   },
   "retina_detect": true
 });
+
+
+
+// all this does is mouse tracking.
+// please let me know if you find this usefull or could make use of it :D
+
+var eye;
+var n_eye = document.querySelector(".eye");
+var n_iris = n_eye.querySelector(".iris");
+var size = n_eye.clientWidth;
+
+(window.onresize = function () {
+  eye = {
+    x: n_eye.offsetLeft + size / 2,
+    y: n_eye.offsetTop + size / 2
+  };
+})();
+
+window.onmouseout = window.onmouseleave = function () {
+  n_iris.setAttribute("class", "iris anim");
+};
+
+window.ontouchend = function (e) {
+  if (e.touches.length == 0) window.onmouseout();
+}
+
+window.onmousemove = function (e) {
+  n_iris.setAttribute("class", "iris");
+  var m = {
+    x: e.clientX - eye.x,
+    y: e.clientY - eye.y
+  };
+  m.rx = 0 - m.y;
+  m.ry = m.x;
+  ["rx", "ry"].forEach(function (r) {
+    m[r] *= 0.1;
+    m[r] = Math.max(-60, m[r]);
+    m[r] = Math.min(60, m[r]);
+  });
+  n_iris.style.transform = "rotateX("+m.rx+"deg) rotateY("+m.ry+"deg) translateZ(68px) scale(0.6)";
+};
+
+window.ontouchmove = window.ontouchstart = function (e) {
+  window.onmousemove(e.touches[0]);
+};
+
+
+/*##########################Google Maps################################*/
+      function initMap() {
+        var uluru = {lat:43.632932, lng:-79.933583 };
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+
